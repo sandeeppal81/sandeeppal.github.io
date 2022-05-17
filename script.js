0b94ec613a8e0e8c725f8a03381428443de986cb
sandeeppal81 <65900914+sandeeppal81@users.noreply.github.com>
2022-05-17 16:10:37 +0530

    finale

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const hemburgerclick = document.querySelector('.hemburger-click');
 
 hemburgerclick.addEventListener('click',()=>{
     hemubergerMenu.classList.toggle('show');
-    console.log("click")
+
 })
